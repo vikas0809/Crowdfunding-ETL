57e71de98489239207a0347f8e238d2e8be2a37c
vikas0809 <vikasjoshi93@live.com>
2022-10-17 17:24:37 -0400

    Added files and data

---
+++ Resources/Queries/crowdfunding_SQL_Analysis.sql
@@ -0,0 +1,54 @@
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+-- 
+SELECT SUM(backers_count) as "count" ,cf_id
+FROM campaign
+WHERE outcome='live'
+GROUP BY cf_id
+ORDER BY "count" DESC;
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+-- 
+SELECT COUNT(b.backer_id) as "backer_count" ,b.cf_id
+FROM backers as b
+INNER JOIN campaign  as camp
+ON camp.cf_id = b.cf_id
+WHERE camp.outcome = 'live'
+GROUP BY b.cf_id
+ORDER BY "backer_count" DESC;
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+
+SELECT ct.first_name,ct.last_name,ct.email,cmp.goal-cmp.pledged as "Amount_Left"
+INTO live_campaigns_contacts
+FROM contacts as ct
+INNER JOIN campaign as cmp
+ON ct.contact_id=cmp.contact_id
+WHERE cmp.outcome='live'
+ORDER BY "Amount_Left" DESC;
+
+-- Check the table
+
+SELECT * FROM live_campaigns_contacts;
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+
+SELECT  bkr.cf_id,bkr.first_name,bkr.last_name,bkr.email,
+		cmp.company_name,cmp.description,cmp.end_date,
+		cmp.goal-cmp.pledged as "Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM campaign as cmp
+INNER JOIN backers as bkr
+ON bkr.cf_id = cmp.cf_id
+ORDER BY bkr.email DESC;
+
+-- Check the table
+
+SELECT * FROM email_backers_remaining_goal_amount;
---
+++ Resources/Queries/crowdfunding_db_ERD.sql
@@ -0,0 +1,40 @@
+campaign
+-
+cf_id int PK
+contact_id int FK - contacts.contact_id
+company_name VARCHAR(100)
+description text
+goal numeric(10,2)
+pledged numeric(10,2)
+outcome VARCHAR(50)
+backers_count INT
+country VARCHAR(20)
+currency VARCHAR(10)
+launched_date DATE
+end_date DATE
+category_id VARCHAR(10) FK - category.category_id
+subcategory_id VARCHAR(10) FK - subcategory.subcategory_id
+
+category
+-
+category_id VARCHAR(10) PK
+category_name VARCHAR(50)
+
+subcategory
+-
+subcategory_id VARCHAR(10) PK
+subcategory_name VARCHAR(50)
+
+contacts
+-
+contact_id INT PK
+first_name VARCHAR(50)
+last_name VARCHAR(50)
+email VARCHAR(100)
+
+backers
+-
+backer_id PK varchar(5)
+cf_id FK - campaign.cf_id
+first_name varchar(50)
+lst_name varchar(50)
